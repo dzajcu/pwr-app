ddb949d4ae902cb7ce2cb1b880f9254504902baf
RussetFox <102963633+RussetFox@users.noreply.github.com>
2023-03-14 02:14:41 +0100

    Podstawowy backend z zależnościami JPA, MySQL, MySQL Server, Web, SB DevTools, Lombok

---
+++ Wirtualna_Politechnika_Back/src/test/java/com/wirt_pol/wirtualna_politechnika_back/WirtualnaPolitechnikaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wirt_pol.wirtualna_politechnika_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WirtualnaPolitechnikaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
