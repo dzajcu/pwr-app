370c56262bd999fd3eaa257028f45e08e9704726
jakub-kowalski <258959@student.pwr.edu.pl>
2023-05-31 18:41:46 +0200

    xd

--- react/src/functions/fetchPostUser.jsx
+++ react/src/functions/fetchPostUser.jsx
@@ -1,11 +1,11 @@
 export const fetchPostUser = (username, email, password) => {
-	fetch('http://localhost:8080/users', {
+	fetch('http://localhost:8080/user/auth/register', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify({
-			username: username,
+			userName: username,
 			email: email,
 			password: password,
 		}),
